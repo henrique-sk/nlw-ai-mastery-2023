960f2597d9cd444790e55655e51c40fd47b359d8
Henrique Schilling Klein <henrique.sklein@gmail.com>
2023-09-11 23:11:55 -0300

    Update inicio do projeto

---
+++ upload-ai-web/src/App.tsx
@@ -0,0 +1,30 @@
+import {Github} from 'lucide-react';
+import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
+
+export function App() {
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤ no NLW da Rocketseat
+          </span>
+
+          <Separator orientation='vertical' className='h-6' />
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <div className="bg-red-500">oi</div>
+
+      {/* 43:16 */}
+    </div>
+  );
+}
